db7038276811e073b0d2a83fdac664aa9a759f85
DDRRDDDD <youngmin4018@gmail.com>
2023-04-21 12:56:09 +0900

    커밋 안된거 커밋하기

---
+++ erpSystem/src/main/java/controller/action/GetBoardListAction.java
@@ -0,0 +1,47 @@
+package controller.action;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import board.Board;
+import board.controller.BoardDAO;
+import boardCategory.controller.BoardCategoryDAO;
+
+public class GetBoardListAction implements Action {
+
+    @Override
+    public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        int categoryId = Integer.parseInt(request.getParameter("cate"));
+
+        BoardDAO boardDao = BoardDAO.getInstance();
+        int total = boardDao.getTotalCountByCategory(categoryId);
+        int lastPage = (int) Math.ceil((double) total / 10);
+
+        String vpage = request.getParameter("vpage");
+
+        if (vpage == null)
+            vpage = "1";
+
+        int selPage = Integer.parseInt(vpage);
+
+        ArrayList<Board> boardList = boardDao.getPostsPerPage(categoryId, selPage);
+        
+        BoardCategoryDAO boardcateDao = BoardCategoryDAO.getInstance();
+        
+        String categroryName = boardcateDao.getCategoryNameById(categoryId);
+
+        request.setAttribute("boards", boardList);
+        request.setAttribute("lastPage", lastPage);
+        request.setAttribute("vpage", vpage);
+        request.setAttribute("category", categroryName);
+        request.setAttribute("categoryId", categoryId);
+        
+        request.setAttribute("content", "board");
+        request.getRequestDispatcher("/").forward(request, response);
+        
+    }
+}
